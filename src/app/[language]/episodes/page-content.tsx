561eab69eeb7843e60c7cb3b17ea109c1f55e596
Gerardo Camorlinga Jr <gercamjr.dev@gmail.com>
2024-09-21 13:50:33 -0500

    refactor(episodes): created RSS feed episode list

--- src/app/[language]/episodes/page-content.tsx
+++ src/app/[language]/episodes/page-content.tsx
@@ -1,37 +1,52 @@
 "use client";
 import { useEffect, useState } from "react";
-import Container from "@mui/material/Container";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import Avatar from "@mui/material/Avatar";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Card from "@mui/material/Card";
+import { styled } from "@mui/material/styles";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import CardMedia from "@mui/material/CardMedia";
 
 // create an episode type with nullable values
 type Episode = {
-  title: string | null;
-  description: string | null;
-  link: string | null;
-  pubDate: string | null;
-  image: string | null;
+  title?: string;
+  description?: string;
+  link?: string;
+  pubDate?: string;
+  image?: string;
 };
 
 // display a list of podcast episodes fetch from an rss feed
 export default function Episodes() {
   //const { t } = useTranslation("episodes");
   const [episodes, setEpisodes] = useState<Episode[]>([]);
+  const [focusedCardIndex, setFocusedCardIndex] = useState<number | null>(null);
+
+  const handleFocus = (index: number) => {
+    setFocusedCardIndex(index);
+  };
+
+  const handleBlur = () => {
+    setFocusedCardIndex(null);
+  };
+
+  const rssURL: string =
+    process.env.NEXT_PUBLIC_PODCAST_RSS_URL ||
+    "https://anchor.fm/s/ef3de024/podcast/rss";
 
   useEffect(() => {
     // fetch episode list from rss feed https://anchor.fm/s/ef3de024/podcast/rss
     fetchEpisodes();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   function stripHtmlTags(str: string) {
-    return str.replace(/<[^>]*>/g, "").replace(/&#39;/g, "'");
+    return str.replace(/&#39;/g, "'");
   }
 
   const fetchEpisodes = async () => {
-    const response = await fetch("https://anchor.fm/s/ef3de024/podcast/rss");
+    const response = await fetch(rssURL);
     const text = await response.text();
     const parser = new DOMParser();
     const xml = parser.parseFromString(text, "text/xml");
@@ -40,41 +55,113 @@ export default function Episodes() {
       return;
     }
     const episodes: Episode[] = Array.from(items).map((item) => {
+      console.log(item);
       return {
-        title: item?.querySelector("title")?.textContent ?? null,
+        title: item?.querySelector("title")?.textContent ?? "",
         description:
           stripHtmlTags(
             item?.querySelector("description")?.textContent || ""
-          ) ?? null,
-        link: item?.querySelector("link")?.textContent ?? null,
-        pubDate: item?.querySelector("pubDate")?.textContent ?? null,
+          ) ?? "",
+        link: item?.querySelector("link")?.textContent ?? "",
+        pubDate: item?.querySelector("pubDate")?.textContent ?? "",
         image:
           item?.getElementsByTagName("itunes:image")[0]?.getAttribute("href") ??
-          null,
+          "",
       };
     });
     setEpisodes(episodes);
   };
 
+  const StyledCard = styled(Card)(({ theme }) => ({
+    display: "flex",
+    flexDirection: "column",
+    padding: 0,
+    height: "100%",
+    backgroundColor: theme.palette.background.paper,
+    "&:hover": {
+      backgroundColor: "transparent",
+      cursor: "pointer",
+    },
+    "&:focus-visible": {
+      outline: "3px solid",
+      outlineColor: "hsla(210, 98%, 48%, 0.5)",
+      outlineOffset: "2px",
+    },
+  }));
+
+  const StyledCardContent = styled(CardContent)(({ theme }) => ({
+    display: "flex",
+    flexDirection: "column",
+    gap: 4,
+    padding: theme.spacing(2),
+    flexGrow: 1,
+    "&:last-child": {
+      paddingBottom: theme.spacing(2),
+    },
+  }));
+
+  const StyledTypography = styled(Typography)({
+    display: "-webkit-box",
+    WebkitBoxOrient: "vertical",
+    WebkitLineClamp: 2,
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+  });
+
   // iterate through the episodes and display episodes in MUI list
   return (
-    <Container>
-      <List>
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 4 }}>
+      <div>
+        <Typography variant="h1" gutterBottom>
+          Podcast Episodes
+        </Typography>
+        <Typography>Stay in the loop with our film reviews</Typography>
+      </div>
+
+      <Grid container spacing={2} columns={12}>
         {episodes.map((episode, index) => (
-          <ListItem key={index}>
-            <ListItemAvatar>
-              {episode.image && (
-                <Avatar
-                  src={episode.image}
-                  alt={episode.title ?? "Episode image"}
-                />
-              )}
-            </ListItemAvatar>
-            <ListItemText primary={episode.title} secondary={episode.pubDate} />
-            <ListItemText primary={episode.description} />
-          </ListItem>
+          <Grid item key={index} xs={12} md={4}>
+            <StyledCard
+              variant="outlined"
+              onFocus={() => handleFocus(0)}
+              onBlur={handleBlur}
+              tabIndex={0}
+              className={focusedCardIndex === 0 ? "Mui-focused" : ""}
+            >
+              <CardMedia
+                component="img"
+                alt="cover art"
+                image={episode.image}
+                aspect-ratio="16 / 9"
+                sx={{
+                  borderBottom: "1px solid",
+                  borderColor: "divider",
+                }}
+              />
+              <StyledCardContent>
+                <Typography gutterBottom variant="caption" component="div">
+                  {episode.pubDate}
+                </Typography>
+                <Typography gutterBottom variant="h6" component="div">
+                  {episode.title}
+                </Typography>
+                <StyledTypography
+                  variant="body2"
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  <p
+                    className="description"
+                    dangerouslySetInnerHTML={{
+                      __html: episode.description || "",
+                    }}
+                  />
+                </StyledTypography>
+              </StyledCardContent>
+            </StyledCard>
+          </Grid>
         ))}
-      </List>
-    </Container>
+      </Grid>
+    </Box>
   );
 }
